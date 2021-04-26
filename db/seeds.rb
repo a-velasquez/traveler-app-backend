1cb879bf2398e5852d35d6c703282957b8a55f13
Alex Velazquez <avelasquez075@gmail.com>
2021-04-26 15:49:49 -0700

    seed db with trip, visit and comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create(name: "Alex", username: "Alex123", password: "123")
+# user = User.create(name: "Alex", username: "Alex123", password: "123")
+
+# alex = User.first 
+
+# alex_trip = alex.trips.create(name: "Tar Pit Hangout", start_date: "2021-07-04", end_date: "2021-07-10")
+
+# alex_trip.visits.create(attraction_id: 1, date: "2021-07-04")
+
+# alex.comments.create(content: "Best pits in town!", user_id: 1, attraction_id: 1)
